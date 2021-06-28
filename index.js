0b8806e6d93d636c223c7ec39804c9d424481a6e
tiko <tiko.pb@gmail.com>
2021-06-28 21:36:23 +0700

    final of chapter 5

--- index.js
+++ index.js
@@ -52,33 +52,56 @@ app.get('/api/v1/user/:id', (req, res) => {
 })
 
 //post data
-app.get('/api/v1/user/post', (req, res) => {
+app.post('/api/v1/user/posts', (req, res) => {
     const { username, password, fullname, email } = req.body
     //Get last of ID
     const id = users[users.length - 1].id + 1
-    const userNew = {
+    const user = {
         id, username, password, fullname, email
     }
-    //save on users json 
-    users.push(userNew)
-    //show the user input parameter
-    res.status(201).json(userNew)
+    users.push(user)
+    console.log(users)
+    
+    users = JSON.stringify(users); // use for parsing const into json file 
+    fs.writeFileSync("./data/user.json", users, "utf8"); // use for saving the json file.
+    
+    res.status(201).json(users)
 })
+
+//put data (digunakan untuk rubah data atau insert jika blm pernah ada)
+app.put('/api/v1/user/:id', (req,res) => {
+    let user = users.filter(i => i.id == req.params.id)
+    //"username":"tiko","password":1234,"fullname":"tiko tiko","email":"tiko.pb@gmail.com"
+
+    const params = { username: req.body.username, password: req.body.password, 
+        fullname: req.body.fullname, email: req.body.email}
+    user = {...user, ...params}
+    //update 
+    users = users.map(i => i.id == user.id ? user : i)
+    res.status(200).json(user)
+})
+
+//delete data 
+app.delete('/api/v1/post/:id', (req,res) => {
+    users = users.filter(i => i.id != req.params.id)
+    res.status(200).json({
+        message: `post dengan id ${req.params.id} berhasil dihapus`
+    })
+});
 
 //login validation
 app.post("/login", (req, res) => {
     const { email, password }  = req.body
     for (var i = 0; i < users.length; i++){
         if(email == users[i].email && password == users[i].password){
-            res.redirect(`/games?name=${users[i].username}`) // if user and password match than redirect into games area 
+            res.redirect(`/games?name=${users[i].usernameY}`) // if user and password match than redirect into games area 
         }
     }
     // if not found then please study about alert! render
     res.render('login')
 });
 
-//register post
-//app post (Create new data)
+//register 
 app.post('/register', (req,res) => {
     const {username, fullname, email, password} = req.body
     //get id 
@@ -92,6 +115,15 @@ app.post('/register', (req,res) => {
     users = JSON.stringify(users); // use for parsing into json file 
     fs.writeFileSync("./data/user.json", users, "utf8"); // use for saving the json file.
     res.render('login')
+})
+
+//erorr handling internal server erorr handler
+app.use(function(err,req,res,next){ 
+    console.error(err)
+    res.status(500).json({
+        status:'fail',
+        erorr:err.message
+    }) 
 })
 
 // 404 handler
