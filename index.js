965e6b886dce1db86e38283cf16f81d5e3dad4dd
tiko <tiko.pb@gmail.com>
2021-06-28 08:29:10 +0700

    adding register page

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require("express")
 
 const app = express()
+const fs = require("fs")
 const port = 8081
 
 app.set('view engine', 'ejs');
@@ -10,8 +11,8 @@ app.use(express.static(__dirname + '/public'));// acess all public folder
 
 //middleware 
 // built in middleware 
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false}));
 
 //middleware
 const router = require('./routing/router')
@@ -21,35 +22,53 @@ app.use(router);
 
 //routing
 //root page
-app.get('/', (req,res) => { 
+app.get('/', (req, res) => {
     res.render('index'); //use for render file: 'view/index.ejs'
 });
 
 //erorr routing 
-app.use(function(err,req,res,next){ 
+app.use(function (err, req, res, next) {
     console.error(err)
     res.status(500).json({
-        status:'fail',
-        erorr:err.message
-    }) 
+        status: 'fail',
+        erorr: err.message
+    })
 })
 
 //api 
 //read all data 
-app.get('/api/v1/user', (req,res) => {
+app.get('/api/v1/user', (req, res) => {
     res.status(200).json(users)
 })
 
 //api get data by id
-app.get('/api/v1/user/:id', (req,res) => {
+app.get('/api/v1/user/:id', (req, res) => {
     const user = users.find(i => i.id == req.params.id)
-    res.status(200).json(user)
+    if (user) {
+        res.status(200).json(user)
+    } else {
+        res.send("NO DATA FOUND")
+    }
+})
+
+//post data
+app.get('api/v1/user/post', (req, res) => {
+    const { username, password, fullname, email } = req.body
+    //Get last of ID
+    const id = users[users.length - 1].id + 1
+    const userNew = {
+        id, username, password, fullname, email
+    }
+    //save on users json 
+    users.push(userNew)
+    //show the user input parameter
+    res.status(201).json(userNew)
 })
 
 // 404 handler
-app.use(function(req,res){
+app.use(function (req, res) {
     res.status(404);
-    res.render('404'); 
+    res.render('404');
 })
 
 //create server start 
--- routing/router.js
+++ routing/router.js
@@ -1,20 +1,26 @@
 const express = require('express')
 const router = express()
 
-router.get("/login", (req,res) => {
+router.get("/login", (req, res) => {
     res.render("login");
     //next(); // -> jika tidak dilakukan fungsi next maka akan berhenti disini. 
 });
 
-router.post("/login", (req,res) => {
+router.post("/login", (req, res) => {
     res.redirect('/games')
 });
 
-router.get("/games", (req,res) => {
+router.get("/register", (req, res) => {
+    res.render("register");
+    //next(); // -> jika tidak dilakukan fungsi next maka akan berhenti disini. 
+});
+
+
+router.get("/games", (req, res) => {
     res.render("games");
 });
 
-router.get("/about", (req,res, next) => {
+router.get("/about", (req, res, next) => {
     res.send("About");
     next(); // -> jika tidak dilakukan fungsi next maka akan berhenti disini. 
 });
